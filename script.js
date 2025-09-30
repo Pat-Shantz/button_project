d4856e66cb349290b1ad9d9722b37e8f9710571c
Pat-Shantz <pat.shantz@gmail.com>
2025-09-30 00:20:39 -0700

    update js and change HTML button text

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 //Script for the confetti feature
-function runConfetti() {
-    confetti(
-        particleCount: 100,
-        spread:70,
-        origin: { y: 0.6 }
-    )
-}
+//function runConfetti() {
+    //confetti(
+        //particleCount: 100,
+        //spread:70,
+        //origin: { y: 0.6 }
+    //)
+//}
 
-const button = document.getElementById('myButton');
+//const button = document.getElementById('myButton');
 
-button.addEventListener('click', runConfetti);
+//button.addEventListener('click', runConfetti);
+
 
 //Script for the counter feature
 let currentCount = 0;
-
 const displayElement = document.getElementById('counter-display');
 const buttonElement = document.getElementById('myButton');
 
 function incrementCounter() {
     //increments the variable
-    currentCount = currentCount++;
+    currentCount += 1;
     //updates HTML display
     displayElement.textContent = currentCount;
     //optional: log the new value to the browser console for debugging (console.log("new count:", currentCount); <-- what is this all about?)
-
 }
 
 buttonElement.addEventListener('click', incrementCounter);
